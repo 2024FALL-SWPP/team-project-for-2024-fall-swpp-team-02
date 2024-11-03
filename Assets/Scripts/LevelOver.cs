f81cf140cb30363d27f3836bc5b27439aa092f5d
dgg1dbg <jeongminx@snu.ac.kr>
2024-11-04 00:58:52 +0900

    implemented basic game over screen

--- Assets/Scripts/LevelOver.cs
+++ Assets/Scripts/LevelOver.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelOver : MonoBehaviour
 {
+    public Button goBackButton;
+    public Button restartButton;
+
+    private int stage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        stage = PlayerPrefs.GetInt("Stage", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene("LevelSelectScene");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Stage" + stage.ToString());
     }
 }
