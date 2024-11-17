742ee163e92b2ba7d4d8c242adf491eb4d816abf
Dongwook Kim <vexatone160@gmail.com>
2024-11-17 20:41:45 +0900

    [#34] Added Trash placing logic, and several prefabs

---
+++ Assets/Scripts/TrashSpawner.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TrashSpawner : MonoBehaviour
+{
+    private GameObject[] _trashes;
+    [SerializeField] private Tilemap trashTilemap;
+    [SerializeField] private TrashMapping[] trashMappings;
+    
+    /// <summary>
+    /// Generates Trash at the given tilemap based position.
+    /// </summary>
+    /// <param name="tilePos">Coordinate on the tilemap</param>
+    public void Spawn(Vector3Int tilePos)
+    {
+        var tile = trashTilemap.GetTile(tilePos);
+        var tileObject = GetMatch(tile);
+        if (tileObject == null) return;
+        
+        var offset = Quaternion.Euler(90f, 0f, 0f) * trashTilemap.cellSize / 2;
+        var objectPos = trashTilemap.CellToWorld(tilePos) + offset;
+        
+        Instantiate(tileObject, objectPos, Quaternion.identity);
+    }
+
+    /// <summary>
+    /// Generates trash at start. Please do not call if it's not the start of level.
+    /// </summary>
+    public void InitialSpawn()
+    {
+        var markerPosEnumerator = trashTilemap.cellBounds.allPositionsWithin;
+        
+        foreach (var marker in markerPosEnumerator)
+            Spawn(marker);
+    }
+
+    private GameObject GetMatch(TileBase marker)
+    {
+        if (marker == null) return null;
+        foreach (var mapping in trashMappings)
+            if (mapping.Match(marker)) return mapping.GetRandomPrefab();
+
+        return null;
+    }
+
+    private void Start()
+    {
+        InitialSpawn();
+        trashTilemap.GetComponent<TilemapRenderer>().enabled = false;
+    }
+}
