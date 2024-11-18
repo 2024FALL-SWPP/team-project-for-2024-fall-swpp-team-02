ab8d461523def02221d556fef0882ead3131de22
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2024-11-18 10:27:45 +0900

    Fix import

--- Assets/Scripts/LevelClear.cs
+++ Assets/Scripts/LevelClear.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
