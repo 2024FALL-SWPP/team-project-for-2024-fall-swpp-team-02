4ab3c299f2b77d0063e95b22698434fe269174cd
dgg1dbg <jeongminx@snu.ac.kr>
2024-11-26 23:26:54 +0900

    UI Script 추가

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -50,4 +50,19 @@ public class StageManager : MonoBehaviour
         int level = DataManager.Instance.GetActiveLevelData().level;
         return goalZList[level-1];
     }
+
+    public bool IsPaused()
+    {
+        return Time.timeScale == 0;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
 }
