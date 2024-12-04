38193593a86344956ef9a1f277ea90c733e6c1e5
김준원 <junis3@snu.ac.kr>
2024-12-04 15:52:10 +0900

    [#86] 각 Scene별로 배경음악 재생되도록, Sound resource 위치 조정, SFX 재생 방법 개선

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
@@ -9,8 +11,10 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource musicSource;
     [SerializeField] private AudioSource sfxSource;
 
-    [SerializeField] private AudioClip musicClip;
-    [SerializeField] private List<AudioClip> sfxClips;
+    private List<AudioClip> musicClips;
+
+    private string currentSceneName;
+
 
     private void Awake()
     {
@@ -27,16 +31,79 @@ public class AudioManager : MonoBehaviour
 
     public void Start()
     {
-        if (musicClip)
+        // Get all music assets in project
+        musicClips = new List<AudioClip>(Resources.LoadAll<AudioClip>("Sounds"));
+
+        SceneManager.activeSceneChanged += SceneChange;
+
+        PlaySceneMusic(SceneManager.GetActiveScene());
+
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    private enum SceneType
+    {
+        Main, Stage, Other
+    };
+
+    SceneType getSceneType(string sceneName)
+    {
+        if (sceneName.StartsWith("Level"))
         {
-            musicSource.clip = musicClip;
-            musicSource.Play();
+            if (sceneName == "LevelSelectScene")
+                return SceneType.Main;
+            if (sceneName == "LevelClearScene" || sceneName == "LevelOverScene")
+                return SceneType.Other;
+            return SceneType.Stage;
         }
+        return SceneType.Main;
+    }
+
+    private void PlaySceneMusic(Scene scene)
+    {
+        switch (getSceneType(scene.name))
+        {
+            case SceneType.Main:
+                musicSource.clip = musicClips.Find(clip => clip.name == "MainMusic");
+                break;
+            case SceneType.Stage:
+                musicSource.clip = musicClips.Find(clip => clip.name.StartsWith(scene.name.Substring(0, scene.name.Length - 5)));
+                break;
+            case SceneType.Other:
+                return;
+        }
+        musicSource.Play();
+    }
+
+    private void SceneChange(Scene _, Scene next)
+    {
+        if (musicSource.isPlaying && getSceneType(currentSceneName) == getSceneType(next.name))
+        {
+            currentSceneName = next.name;
+            return;
+        }
+
+        musicSource.Stop();
+        PlaySceneMusic(next);
+
+        currentSceneName = next.name;
     }
 
     public void PlaySFX(string name)
     {
-        int index = sfxClips.FindIndex(clip => clip.name == name);
-        sfxSource.PlayOneShot(sfxClips[index]);
+        var music = musicClips.Find(clip => clip.name == name);
+        sfxSource.PlayOneShot(music);
+    }
+
+    // Coroutine to play sound effect
+    public IEnumerator PlaySFXAfterCoroutine(string sfxName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        PlaySFX(sfxName);
+    }
+
+    public void PlaySFXAfter(string sfxName, float delay)
+    {
+        StartCoroutine(PlaySFXAfterCoroutine(sfxName, delay));
     }
 }
