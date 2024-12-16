22a6a2c08a33148b1bbd08ee5475c4df6cb865fe
dgg1dbg <jeongminx@snu.ac.kr>
2024-12-16 20:24:38 +0900

    ingame manual

---
+++ Assets/Scripts/ShowDescription.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShowDescription : MonoBehaviour
+{
+    public string url = "https://drive.google.com/file/d/1nTj-eee-Gi8TGH2GQml52v0zwNvMv6Ad/view";
+
+    public void Show()
+    {
+            Application.OpenURL(url);
+    }
+}
+
