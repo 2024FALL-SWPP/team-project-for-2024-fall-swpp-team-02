09c98b3082ba61be5e8e21b16af7af4598b9bd52
super-linter <super-linter@super-linter.dev>
2024-12-16 09:16:06 +0000

    chore: fix linting issues

--- Assets/Scripts/UIShake.cs
+++ Assets/Scripts/UIShake.cs
@@ -3,46 +3,46 @@ using UnityEngine;
 
 public class UIShake : MonoBehaviour
 {
-  private RectTransform targetUIElement;
+    private RectTransform targetUIElement;
 
-  private Vector3 originalPosition;
-  private Coroutine currentShakeRoutine;
+    private Vector3 originalPosition;
+    private Coroutine currentShakeRoutine;
 
-  void Start()
-  {
-    if (targetUIElement == null)
-      targetUIElement = GetComponent<RectTransform>();
-    originalPosition = targetUIElement.localPosition;
-  }
-
-  public void Shake(float shakeDuration, float shakeMagnitude)
-  {
-    if (currentShakeRoutine != null)
+    void Start()
     {
-      StopCoroutine(currentShakeRoutine);
-      targetUIElement.localPosition = originalPosition; // 위치 초기화
+        if (targetUIElement == null)
+            targetUIElement = GetComponent<RectTransform>();
+        originalPosition = targetUIElement.localPosition;
     }
 
-    currentShakeRoutine = StartCoroutine(ShakeRoutine(shakeDuration, shakeMagnitude));
-  }
-
-  private IEnumerator ShakeRoutine(float shakeDuration, float shakeMagnitude)
-  {
-    float elapsed = 0f;
-
-    while (elapsed < shakeDuration)
+    public void Shake(float shakeDuration, float shakeMagnitude)
     {
-      float offsetX = Random.Range(-1f, 1f) * shakeMagnitude;
-      float offsetY = Random.Range(-1f, 1f) * shakeMagnitude;
+        if (currentShakeRoutine != null)
+        {
+            StopCoroutine(currentShakeRoutine);
+            targetUIElement.localPosition = originalPosition; // 위치 초기화
+        }
 
-      targetUIElement.localPosition = originalPosition + new Vector3(offsetX, offsetY, 0);
-
-      elapsed += Time.deltaTime;
-
-      yield return null;
+        currentShakeRoutine = StartCoroutine(ShakeRoutine(shakeDuration, shakeMagnitude));
     }
 
-    targetUIElement.localPosition = originalPosition;
-    currentShakeRoutine = null;
-  }
+    private IEnumerator ShakeRoutine(float shakeDuration, float shakeMagnitude)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < shakeDuration)
+        {
+            float offsetX = Random.Range(-1f, 1f) * shakeMagnitude;
+            float offsetY = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            targetUIElement.localPosition = originalPosition + new Vector3(offsetX, offsetY, 0);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        targetUIElement.localPosition = originalPosition;
+        currentShakeRoutine = null;
+    }
 }
